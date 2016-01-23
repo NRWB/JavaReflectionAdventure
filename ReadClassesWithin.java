3d9bde8754e8f9ff4611ae008c00bb07a9cc8685
Nick <NRWB@users.noreply.github.com>
2016-01-22 16:46:24 -0800

    Update ReadClassesWithin.java
    Forgot to rename method ("getAllFiles" vs. "getClasseNames").
    Added method documentation header to the getAllFiles method.
    getAllFiles method now correctly resembles functionality of obtaining all files from a jar file.
    getAllFiles now uses try with resources (uses Java 7 or later).

--- ReadClassesWithin.java
+++ ReadClassesWithin.java
@@ -7,34 +7,34 @@ import java.util.jar.JarInputStream;
 
 public class ReadClassesWithin {
 
-	public static final boolean DEBUG = true;
 	public static final String JAR_PATH = "C:\\Users\\Name\\Desktop\\test.jar";
 
 	public static void main(String[] args) throws Exception {
-		System.out.println(getClasseNames(JAR_PATH));
+		System.out.println(getAllFiles(JAR_PATH));
 		System.out.println();
 
 	}
 
-	public static List<String> getAllFiles(final String str) {
+	/**
+	 * Obtains all the file names contained within the jar file.
+	 * Uses try with resources statement, requires Java 7 or later.
+	 * @param s The file pathname to the specified jar file.
+	 * @return A list of jar file names. 
+	 */
+	public static List<String> getAllFiles(final String s) {
 		List<String> classes = new ArrayList<String>();
-		JarInputStream jarFile = new JarInputStream(new FileInputStream(jarName));
-		while (true) {
-			JarEntry jarEntry = jarFile.getNextJarEntry();
-			if (jarEntry == null)
-				break;
-			if (jarEntry.getName().endsWith(".class"))
-				classes.add(jarEntry.getName().replaceAll("/", "\\."));
-			else
-				System.out.println(jarEntry.getName());
-		}
-		if (jarFile != null) {
-			try {
-				jarFile.close();
-			} catch (IOException e) {
-				e.printStackTrace();
+
+		try (JarInputStream jis = new JarInputStream(new FileInputStream(s))) {
+			while (true) {
+				JarEntry je = jis.getNextJarEntry();
+				if (je == null)
+					break;
+				classes.add(je.getName());
 			}
+		} catch (IOException e) {
+			e.printStackTrace();
 		}
+
 		return classes;
 	}
 }
