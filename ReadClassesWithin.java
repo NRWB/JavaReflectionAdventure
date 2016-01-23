63d10ad7c2df3f0fbec2e41e426ca68f66a943b8
Nick <NRWB@users.noreply.github.com>
2016-01-22 17:01:12 -0800

    Update ReadClassesWithin.java
    Added an additional method, getAllClassFiles - gets all files from the jar file and filters out the files not containing ".class"

--- ReadClassesWithin.java
+++ ReadClassesWithin.java
@@ -10,11 +10,10 @@ public class ReadClassesWithin {
 	public static final String JAR_PATH = "C:\\Users\\Name\\Desktop\\test.jar";
 
 	public static void main(String[] args) throws Exception {
-		System.out.println(getAllFiles(JAR_PATH));
-		System.out.println();
-
+		System.out.println(getAllFiles(JAR_PATH).size());
+		System.out.println(getAllClassFiles(JAR_PATH).size());
 	}
-
+	
 	/**
 	 * Obtains all the file names contained within the jar file.
 	 * Uses try with resources statement, requires Java 7 or later.
@@ -22,19 +21,36 @@ public class ReadClassesWithin {
 	 * @return A list of jar file names. 
 	 */
 	public static List<String> getAllFiles(final String s) {
-		List<String> classes = new ArrayList<String>();
+		List<String> list = new ArrayList<String>();
 
 		try (JarInputStream jis = new JarInputStream(new FileInputStream(s))) {
 			while (true) {
 				JarEntry je = jis.getNextJarEntry();
 				if (je == null)
 					break;
-				classes.add(je.getName());
+				list.add(je.getName());
 			}
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
 
-		return classes;
+		return list;
 	}
+	
+	/**
+	 * Obtains all the java class file names contained within the jar file.
+	 * @param s The file pathname to the specified jar file.
+	 * @return A list of jar file names. 
+	 */
+	public static List<String> getAllClassFiles(final String s) {
+		List<String> list = getAllFiles(s);
+		List<String> result = new ArrayList<String>();
+
+		for (String str : list)
+			if (str.contains(".class"))
+				result.add(str);
+
+		return result;
+	}
+
 }
